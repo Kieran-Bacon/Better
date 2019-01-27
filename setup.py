ded8375923a16ec10107d76a6a1a88c51178b253
Kieran Bacon <Kieran.Bacon@outlook.com>
2019-01-27 15:11:30 +0000

    update the setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "README.md")),
       README = handler.read()
 requires = []
 
-setup(name='better',
-      install_requires=requires,
-      version="0.1.1",
-      description="Extend packages include additional desired features",
-      long_description=README,
-      long_description_content_type="text/markdown",
-
-      author="Kieran Bacon",
-      author_email="Kieran.Bacon@outlook.com",
-      url="https://github.com/Kieran-Bacon/Better",
-
-      packages=find_packages()
+setup(
+    name='better',
+    install_requires=requires,
+    version="0.1.1",
+    description="Extend packages include additional desired features",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Kieran Bacon",
+    author_email="Kieran.Bacon@outlook.com",
+    url="https://github.com/Kieran-Bacon/Better",
+    packages=find_packages(),
+    classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License"
+    ]
 )
