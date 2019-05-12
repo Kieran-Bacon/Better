608fccd071e9b748f07f8cd402639168b1ee248f
Kieran Bacon <kieran.bacon@outlook.com>
2019-05-12 20:55:11 +0100

    update setup and changelog.md

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = []
 setup(
     name='better',
     install_requires=requires,
-    version="0.2.1",
+    version="0.3.0",
     description="Extend packages include additional desired features",
     long_description=README,
     long_description_content_type="text/markdown",
